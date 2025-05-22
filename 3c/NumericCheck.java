2069ee41ef60cc54959fc60a700a959cbfb3fd58
Apoorvaguttedar <155739194+Apoorvaguttedar@users.noreply.github.com>
2025-05-22 07:12:44 +0600

    Update NumericCheck.java

--- 3c/NumericCheck.java
+++ 3c/NumericCheck.java
@@ -1,3 +1,7 @@
+/*Write a Java Program for Verifying if a string contains only numeric 
+characters using user defined  function isNumeric()*/
+
+
 package JavaCollection;
 
 import java.util.Scanner;
