5f0985dd76c457d2779e715f6323535dc1c73af0
Apoorvaguttedar <155739194+Apoorvaguttedar@users.noreply.github.com>
2025-05-22 07:12:08 +0600

    Update TruncateString.java

--- 3c/TruncateString.java
+++ 3c/TruncateString.java
@@ -1,3 +1,8 @@
+/* Write a Java Program for Shortening a string to a specified length and adds an
+ellipsis using user  defined function truncate() */
+
+
+
 package JavaCollection;
 
 import java.util.Scanner;
